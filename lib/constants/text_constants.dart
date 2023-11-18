ad0694a265e987bffb283b5ef460de39a3886f07
Balogun Murtadho <murtadhob@gmail.com>
2023-11-19 00:43:36 +0100

    Created a variable constants for texts for code readability and maintainability

---
+++ lib/constants/text_constants.dart
@@ -0,0 +1,29 @@
+class CalculatorAppTexts {
+  static const String appName = 'Calculator App';
+  static const String appOwner = "Balogun Murtadho";
+
+  // Calcuator Screen
+  static const String clear = 'C';
+  static const String delete = '⌫';
+  static const String percentage = '%';
+  static const String divided = '/';
+  static const String multiply = '*';
+  static const String minus = '-';
+  static const String plus = '+';
+  static const String equal = '=';
+  static const String dot = '.';
+  static const String zero = '0';
+  static const String one = '1';
+  static const String two = '2';
+  static const String three = '3';
+  static const String four = '4';
+  static const String five = '5';
+  static const String six = '6';
+  static const String seven = '7';
+  static const String eight = '8';
+  static const String nine = '9';
+  static const String doubleZero = '00';
+
+  // Calculator Functions
+  static const String emptyString = '';
+}
