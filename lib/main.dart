106932d79ea76425c6b9c4aaf25604586d10b5e8
Balogun Murtadho <murtadhob@gmail.com>
2023-11-19 00:44:49 +0100

    Main file updated

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:calculator_app/constants/assets_path.dart';
+import 'package:calculator_app/constants/text_constants.dart';
+import 'package:tbib_splash_screen/splash_screen_view.dart';
+import 'package:calculator_app/screens/calculator_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isLoaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  Future<void> _loadData() async {
+    await Future.delayed(const Duration(seconds: 4));
+    setState(() {
+      _isLoaded = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: CalculatorAppTexts.appName,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: CalculatorAppAssetsPath.fonts,
+      ),
+      home: SplashScreenView(
+        navigateWhere: _isLoaded,
+        navigateRoute: const CalculatorScreen(),
+        text: WavyAnimatedText(
+          CalculatorAppTexts.appOwner,
+          textStyle: const TextStyle(
+            color: Colors.blue,
+            fontSize: 32.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        imageSrc: CalculatorAppAssetsPath.appOwner,
+      ),
+    );
+  }
+}
